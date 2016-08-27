63c62271e9f5dd17f0acc43fad1405d131885893
reorx <novoreorx@gmail.com>
2016-08-27 20:51:10 +0800

    remove oauth2 as dependency, use requests_oauthlib to do pin oauth

--- ts/auth.py
+++ ts/auth.py
@@ -1,9 +1,11 @@
 # coding: utf-8
 
 import urlparse
-import oauth2 as oauth
+import requests
+from requests_oauthlib import OAuth1
 from .config import get_config
 from .log import lg
+from . import color
 
 
 class OauthError(Exception):
@@ -20,34 +22,42 @@ def get_oauth_token():
     access_token_url = 'https://api.twitter.com/oauth/access_token'
     authorize_url = 'https://api.twitter.com/oauth/authorize'
 
-    consumer = oauth.Consumer(consumer_key, consumer_secret)
-    client = oauth.Client(consumer)
+    # consumer = oauth.Consumer(consumer_key, consumer_secret)
+    # client = oauth.Client(consumer)
+    oauth = OAuth1(consumer_key, client_secret=consumer_secret)
 
     # Step 1: Get a request token
-    resp, content = client.request(request_token_url, "GET")
-    if resp['status'] != '200':
-        raise OauthError('Invalid response {}.'.format(resp['status']))
 
-    request_token = dict(urlparse.parse_qsl(content))
+    resp = requests.post(request_token_url, auth=oauth)
+    if resp.status_code != 200:
+        raise OauthError(
+            'Invalid response on request token {} {}.'.format(resp.status_code, resp.content))
+    request_token = dict(urlparse.parse_qsl(resp.content))
     lg.debug('Request token (oauth_token, oauth_token_secret): %s, %s',
              request_token['oauth_token'], request_token['oauth_token_secret'])
 
     # Step 2: Redirect to the provider
-    print 'Go to the following link in your browser:'
-    print '%s?oauth_token=%s' % (authorize_url, request_token['oauth_token'])
+    print color.blue('Go to the following link in your browser:')
+    print color.underline('%s?oauth_token=%s' % (authorize_url, request_token['oauth_token']))
     print
 
-    oauth_verifier = raw_input('Enter PIN: ')
+    verifier = raw_input('Enter PIN: ')
+    print
 
     # Step 3: get access token & secret
-    token = oauth.Token(
-        request_token['oauth_token'],
-        request_token['oauth_token_secret'])
-    token.set_verifier(oauth_verifier)
-    client = oauth.Client(consumer, token)
+    oauth = OAuth1(
+        consumer_key,
+        client_secret=consumer_secret,
+        resource_owner_key=request_token['oauth_token'],
+        resource_owner_secret=request_token['oauth_token_secret'],
+        verifier=verifier)
 
-    resp, content = client.request(access_token_url, "POST")
-    access_token = dict(urlparse.parse_qsl(content))
+    resp = requests.post(access_token_url, auth=oauth)
+    if resp.status_code != 200:
+        raise OauthError(
+            'Invalid response on access token {} {}.'.format(resp.status_code, resp.content))
+
+    access_token = dict(urlparse.parse_qsl(resp.content))
     access_key = access_token['oauth_token']
     access_secret = access_token['oauth_token_secret']
 
